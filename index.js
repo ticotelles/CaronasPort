ac7e87719019f995fe572d4c7e91be3a224b8e9d
Arthur Telles <arthurtelles19@gmail.com>
2023-09-06 13:56:00 -0300

    criando pag

---
+++ index.js
@@ -0,0 +1,15 @@
+function login() {
+    window.location.href = "Pages/Home/home.html";
+}
+
+function voltar() {
+    window.location.href = "Pages/Login/login.html";
+}
+
+function cadastrar() {
+    window.location.href = "cadastro.html";
+}
+
+function sair() {
+    window.location.href = "login.html";
+}
