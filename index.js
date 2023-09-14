eac3fa805f10bf4c7d440476a2455e577e23ddee
Arthur Telles <arthurtelles19@gmail.com>
2023-09-13 23:06:56 -0300

    validação do form part1

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-function login() {
-    window.location.href = "Pages/Home/home.html";
-}
+// function login() {
+//     window.location.href = "Pages/Home/home.html";
+// }
 
-function voltar() {
-    window.location.href = "Pages/Login/login.html";
-}
+// function voltar() {
+//     window.location.href = "Pages/Login/login.html";
+// }
 
-function cadastrar() {
-    window.location.href = "cadastro.html";
-}
+// function cadastrar() {
+//     window.location.href = "cadastro.html";
+// }
 
-function sair() {
-    window.location.href = "login.html";
-}
+// function sair() {
+//     window.location.href = "login.html";
+// }
