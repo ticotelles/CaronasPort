300d69d1d0cfd7136532f5a8570f369eaec81593
Arthur Telles <arthurtelles19@gmail.com>
2023-09-14 16:15:57 -0300

    fluxo das pag

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
-// function login() {
-//     window.location.href = "Pages/Home/home.html";
-// }
+function login() {
+  window.location.href =
+    "file:///C:/workspace/Carona%20Port/Pages/Home/home.html";
+}
 
-// function voltar() {
-//     window.location.href = "Pages/Login/login.html";
-// }
+function cadastrar() {
+  window.location.href =
+    "file:///C:/workspace/Carona%20Port/Pages/Cadastro/cadastro.html";
+}
 
-// function cadastrar() {
-//     window.location.href = "cadastro.html";
-// }
+function voltar() {
+  window.location.href = "Pages/Login/login.html";
+}
 
-// function sair() {
-//     window.location.href = "login.html";
-// }
+function cadastrarCarona() {
+  window.location.href =
+    "file:///C:/workspace/Carona%20Port/Pages/CadastroCarona/cadastroCarona.html";
+}
+
+function caronaCadastrada() {
+  window.location.href =
+    "file:///C:/workspace/Carona%20Port/Pages/CaronaCadastrado/caronaCadastrado.html";
+}
+
+function sair() {
+  window.location.href = "login.html";
+}
+
+function publicacao() {
+  window.location.href =
+    "file:///C:/workspace/Carona%20Port/Pages/Publicacao/publicacao.html";
+}
