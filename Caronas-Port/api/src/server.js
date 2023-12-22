328b271636650425287eeeeb750817b66ef98ad1
Arthur Telles <arthurtelles19@gmail.com>
2023-12-22 17:00:25 -0300

    tabela users

--- Caronas-Port/api/src/server.js
+++ Caronas-Port/api/src/server.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const cors = require("cors");
-
+const knex = require("./database");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+
+
+(async () => {
+    if ((await knex.schema.hasTable("users")) == false) {
+      await knex.schema.createTable("users", function (table) {
+        table.increments("id").primary();
+        table.string("name");
+        table.string("email");
+        table.string("whats");
+        table.string("city");
+        table.string("eventId");
+        table.timestamps(true, true);
+      });
+      console.log("criado a tabela users");
+    }
+})();
+
+
 
 
 
