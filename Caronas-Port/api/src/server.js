71e9617a8b0d9f8de07e86586dbc1db9624fdab3
Arthur Telles <arthurtelles19@gmail.com>
2024-01-19 16:36:24 -0300

    tryCatch

--- Caronas-Port/api/src/server.js
+++ Caronas-Port/api/src/server.js
@@ -1,117 +1,164 @@
 const express = require("express");
 const cors = require("cors");
 const knex = require("./database");
-
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-
-
 (async () => {
-    if ((await knex.schema.hasTable("users")) == false) {
-      await knex.schema.createTable("users", function (table) {
-        table.increments("id").primary();
-        table.string("name");
-        table.string("email");
-        table.string("whats");
-        table.string("city");
-        table.string("eventId");
-        table.timestamps(true, true);
-      });
-      console.log("criado a tabela users");
-    }
+  if ((await knex.schema.hasTable("users")) == false) {
+    await knex.schema.createTable("users", function (table) {
+      table.increments("id").primary();
+      table.string("name");
+      table.string("email");
+      table.string("telephone");
+      table.date("date_birth");
+    });
 
-    if ((await knex.schema.hasTable("offerRide")) == false) {
-        await knex.schema.createTable("offerRide", function (table) {
-          table.increments("id").primary();
-          table.string("origin");
-          table.string("destination");
-          table.dateTime("time");
-          table.integer("price");
-          table.date("date");
-          table.integer("telephone");
-          table.string("description");
-        });
-        console.log("criado tabela offerRide");
-      }
-    
-      if ((await knex.schema.hasTable("requestRide")) == false) {
-        await knex.schema.createTable("requestRide", function (table) {
-          table.increments("id").primary();
-          table.string("origin");
-          table.string("destination");
-          table.dateTime("time");
-          table.date("date");
-          table.integer("telephone");
-          table.string("description");
-        });
-      }
-      console.log("criado a tabela requestRide");
+    console.log("criado a tabela users");
+  }
+
+  if ((await knex.schema.hasTable("offerRide")) == false) {
+    await knex.schema.createTable("offerRide", function (table) {
+      table.increments("id").primary();
+      table.string("origin");
+      table.string("destination");
+      table.dateTime("time");
+      table.integer("price");
+      table.date("date");
+      table.integer("telephone");
+      table.string("description");
+    });
+    console.log("criado tabela offerRide");
+  }
+
+  if ((await knex.schema.hasTable("requestRide")) == false) {
+    await knex.schema.createTable("requestRide", function (table) {
+      table.increments("id").primary();
+      table.string("origin");
+      table.string("destination");
+      table.dateTime("time");
+      table.date("date");
+      table.integer("telephone");
+      table.string("description");
+    });
+  }
+  console.log("criado a tabela requestRide");
 })();
 
 app.listen("3333", function () {
-    console.log("server rodando");
+  console.log("server rodando");
 });
-
-
 
 /*--------------------------USERS----------------------------*/
 app.post("/users", function (req, res) {
-    res.send("Usuario Criado");
-  });
+  try {
+    res.status(201).json({ status: "ok"});
+  } catch (error) {
+    console.log(error)
+  }
   
-  app.get("/users", function (req, res) {
-    res.send("GetAll");
-  });
-  
-  app.put("/users", function (req, res) {
-    res.send("Atualizando usuário");
-  });
-  
-  app.delete("/users", function (req, res) {
-    res.send("usuário deletado");
-  });
-  
-  /*--------------------------search----------------------------*/
-  
-  app.get("/search", function (req, res) {
-    res.send("pegando publicações");
-  });
-  
-  /* -------------------------offer---------------------------*/
-  
-  app.post("/offer", function (req, res) {
-    res.send("oferecer carona Criado");
-  });
-  
-  app.get("/offer", function (req, res) {
-    res.send("pegando ofertas de Carona");
-  });
-  
-  app.put("/offer", function (req, res) {
-    res.send("atualizando ofertas");
-  });
-  
-  app.delete("/offer", function (req, res) {
-    res.send("deletendo oferta");
-  });
-  
-  /* -------------------------request---------------------------*/
-  
-  app.post("/request", function (req, res) {
-    res.send("pedido de carona criado");
-  });
-  
-  app.get("/request", function (req, res) {
-    res.send("pegando os pedidos de carona");
-  });
-  
-  app.put("/request", function (req, res) {
-    res.send("atualizando pedido de carona");
-  });
-  
-  app.delete("/request", function (req, res) {
-    res.send("deletendo pedido de carona");
-  });
+});
+
+app.get("/users", function (req, res) {
+  try {
+    res.status(200).json({ status: "ok" });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.put("/users", function (req, res) {
+  try {
+    res.status(200).json({ status: "ok" });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.delete("/users", function (req, res) {
+  try {
+    res.status(200).json({ status: "ok" });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+/*--------------------------search----------------------------*/
+
+app.get("/search", function (req, res) {
+  try {
+    res.status(200).json({ status: "ok" });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+/* -------------------------offer---------------------------*/
+
+app.post("/offer", function (req, res) {
+  try {
+    res.status(201).json({ status: "ok" });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get("/offer", function (req, res) {
+  try {
+    res.status(200).json({ status: "ok" });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.put("/offer", function (req, res) {
+  try {
+    res.status(200).json({ status: "ok" });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.delete("/offer", function (req, res) {
+  try {
+    res.status(200).json({ status: "ok" });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+/* -------------------------request---------------------------*/
+
+app.post("/request", function (req, res) {
+  try {
+    res.status(201).json({ status: "ok" });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get("/request", function (req, res) {
+  try {
+    res.status(200).json({ status: "ok" });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.put("/request", function (req, res) {
+  try {
+    res.status(200).json({ status: "ok" });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.delete("/request", function (req, res) {
+  try {
+    res.status(200).json({ status: "ok" });
+  } catch (error) {
+    console.log(error);
+  }
+});
