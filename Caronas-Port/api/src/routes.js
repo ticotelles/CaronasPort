3c0c4eaee20a72a86b8159e58ed4fb380fc854ef
Arthur Telles <arthurtelles19@gmail.com>
2024-03-03 15:51:59 -0300

    evolução routes

--- Caronas-Port/api/src/routes.js
+++ Caronas-Port/api/src/routes.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const knex = require("./database");
 const offerController = require("./controllers/ride");
-const requestController = require("./controllers/request");
-const searchController = require("./controllers/search");
+
 const usersController = require("./controllers/users");
 const loginController = require("./controllers/login");
 const appRoutes = express.Router();
@@ -11,14 +10,6 @@ const appRoutes = express.Router();
 appRoutes.post("/users", usersController.create);
 appRoutes.get("/users", usersController.getAll);
 
-
-//ROTAS REQUEST
-appRoutes.post("/request", requestController.create);
-appRoutes.get("/request", requestController.getAll);
-
-
-//ROTAS SEARCH
-appRoutes.get("/search", searchController.getAll);
 
 //ROTAS OFFER
 appRoutes.post("/offer", offerController.create);
