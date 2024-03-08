c3701305586f4857833de3f1fde630926a664416
Arthur Telles <arthurtelles19@gmail.com>
2024-03-08 19:48:22 -0300

    evolução ride

--- Caronas-Port/api/src/controllers/ride.js
+++ Caronas-Port/api/src/controllers/ride.js
@@ -3,41 +3,51 @@ const knex = require("../database");
 class rideController {
   async create(req, res) {
     try {
-      if (!req.body.origin || req.body.origin == "") {
-        return res.status(400).json({
-          status: "ERROR",
-          msg: "O campo Origem é Obrigatório! ",
-        });
-      }
+  
+      // if (!req.body.origin || req.body.origin == "") {
+      //   return res.status(400).json({
+      //     status: "ERROR",
+      //     msg: "O campo origin é Obrigatório! ",
+      //   });
+      // }
 
-      if (!req.body.time || req.body.time == "") {
-        return res.status(400).json({
-          status: "ERROR",
-          msg: "O Horário é Obrigatório! ",
-        });
-      }
+      // if (!req.body.time || req.body.time == "") {
+      //   return res.status(400).json({
+      //     status: "ERROR",
+      //     msg: "O time é Obrigatório! ",
+      //   });
+      // }
 
-      if (!req.body.destiny || req.body.destiny == "") {
-        return res.status(400).json({
-          status: "ERROR",
-          msg: "O campo Destino é Obrigatório! ",
-        });
-      }
+      // if (!req.body.destiny || req.body.destiny == "") {
+      //   return res.status(400).json({
+      //     status: "ERROR",
+      //     msg: "O campo destiny é Obrigatório! ",
+      //   });
+      // }
 
-      if (!req.body.date || req.body.date == "") {
-        return res.status(400).json({
-          status: "ERROR",
-          msg: "O campo Horário é Obrigatório! ",
-        });
-      }
-      if (!req.body.type || req.body.time == "") {
-        return res.status(400).json({
-          status: "ERROR",
-          msg: "O campo Tipo é Obrigatório!",
-        });
-      }
+      // if (!req.body.date || req.body.date == "") {
+      //   return res.status(400).json({
+      //     status: "ERROR",
+      //     msg: "O campo date é Obrigatório! ",
+      //   });
+      // }
+
+      // if (!req.body.type || req.body.time == "") {
+      //   return res.status(400).json({
+      //     status: "ERROR",
+      //     msg: "O campo type é Obrigatório!",
+      //   });
+      // }
+
+      // if (req.body.type !== "offer" && req.body.type !== "request") {
+      //   return res.status(400).json({
+      //     status: "ERROR",
+      //     msg: "O campo type aceita somente 'offer' ou 'request'",
+      //   });
+      // }
 
       const data = {
+        user_id: req.body.user_id,
         origin: req.body.origin,
         destination: req.body.destiny,
         time: req.body.time,
@@ -45,6 +55,7 @@ class rideController {
         observation: req.body.observation,
         type: req.body.type,
       };
+
       await knex("ride").insert(data);
 
       res.status(201).json({ status: "ok" });
@@ -56,7 +67,10 @@ class rideController {
 
   async getAll(req, res) {
     try {
-      const data = await knex("ride");
+      const data = await knex("ride")
+        .join("users", "ride.user_id", "=", "users.id")
+        .select("ride.id", "ride.date", "ride.time", "users.name");
+
       res.status(200).json({ status: "ok", dataRes: data });
     } catch (error) {
       console.log(error);
@@ -66,12 +80,23 @@ class rideController {
 
   async getOne(req, res) {
     try {
-      const dataRide = await knex("ride").where("id", req.params.id).first();
-      const dataUser = await knex.select("name", "vehicle", "telephone").from("users");
-      const data = {
-        dataUser,
-        dataRide
-      };
+      const data = await knex("ride")
+        .where("ride.id", req.params.id)
+        .join("users", "ride.user_id", "=", "users.id")
+        .select(
+          "ride.id",
+          "users.name",
+          "users.vehicle",
+          "users.telephone as phone",
+          "ride.origin",
+          "ride.destination as destiny",
+          "ride.observation",
+          "ride.type",
+          "ride.time",
+          "ride.type",
+          "ride.date"
+        )
+        .first();
 
       if (data === undefined) {
         return res.status(400).json({
